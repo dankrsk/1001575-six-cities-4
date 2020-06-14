bdac1bfbb3d83cbf2bdbbc4c0fa27d6230e149ad
Даниил Г <dankrsk@icloud.com>
2020-06-14 21:00:26 +0700

    Работа с валидацией с помощью prop-types

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,11 +3,12 @@ import Main from '../main/main.js';
 
 const App = (props) => {
   // eslint-disable-next-line react/prop-types
-  const {rentOffersCount} = props;
+  const {rentOffersCount, offers} = props;
 
   return (
     <Main
       rentOffersCount={rentOffersCount}
+      offers={offers}
     />
   );
 };
