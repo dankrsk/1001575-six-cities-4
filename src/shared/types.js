1d5561c085d507714332f903c61393653776e02a
Даниил Г <dankrsk@icloud.com>
2020-06-14 23:07:23 +0700

    Исправление по экспорту

--- src/shared/types.js
+++ src/shared/types.js
@@ -1,11 +1,9 @@
 import PropTypes from 'prop-types';
 
-const OFFER_PROP_TYPES = PropTypes.shape(
+export const OFFER_PROP_TYPES = PropTypes.shape(
     {
       title: PropTypes.string.isRequired,
       price: PropTypes.number.isRequired,
     }
 );
-
-export {OFFER_PROP_TYPES};
 
